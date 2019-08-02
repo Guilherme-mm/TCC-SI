bc3deb406ff11a83e75d955836f0749844bf7f1b
Guilherme Müller <guilherme.muller.m@gmail.com>
2019-08-02 00:04:29 -0300

    Altera formato das mensagens, adiciona controle de parada e novo metodo
    - Altera os métodos existentes para que se adequem ao formato de body esperado pelo serviço daemon
    - Adiciona checagem do tipo de mensagem vinda do daemon. Caso seja de código 3 (exit) a execução é interrompida
    - Adiciona novo método que será utilizado para solicitar alteração do path dos arquivos de logs

--- src/clt/GERTClt.py
+++ src/clt/GERTClt.py
@@ -22,8 +22,10 @@ import socket
 import sys
 import click
 import threading
+import json
 
 from ApiMessage import ApiMessage
+from MessageType import MessageType
 
 service_address = ('python-daemon', 10000)
 clt_address = ('python-clt', 10000)
@@ -34,13 +36,18 @@ sock.bind(clt_address)
 
 
 def SendMessage(message):
-    print('Sending: [{}]'.format(message.getMessageBody()))
+    print('Sending: [{}]...'.format(message.getMessageBody()))
+    print("")
     sock.sendto(message.getMessageBody().encode('utf-8'), (message.getReceiverAddress(), message.getReceiverPort()))
 
 def MessageReceiver():
     while True:
         data, server = sock.recvfrom(4096)
-        print('Response Value: [{}], Origin: [{}]'.format(data, server))
+        data = data.decode('utf-8')
+        messageBody = json.loads(data)
+        if messageBody["messageType"] == MessageType.END.value:
+            print('Response Value: [{}]'.format(data))
+            exit()
 
 @click.group()
 def cli():
@@ -52,6 +59,15 @@ def sendMessageToService(body):
     message = ApiMessage(body, clt_address[0], clt_address[1], service_address[0], service_address[1])
     SendMessage(message)
 
+@cli.command()
+@click.option('--path', help='Location of the log files from where data will be collected')
+def setLogPath(path):
+    messageBody = {}
+    messageBody["operationCode"] = 1 #Set log path
+    messageBody["messageType"] = 1 #Begin
+    messageBody["path"] = path
+    message = ApiMessage(json.dumps(messageBody), clt_address[0], clt_address[1], service_address[0], service_address[1])
+    SendMessage(message)
 
 if __name__ == "__main__":
     threading.Thread(target = MessageReceiver).start()
